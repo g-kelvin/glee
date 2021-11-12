ea4ae9e4bb746985c2e2820de26f9af3c32ab404
g-kelvin <kelvingauki@gmail.com>
2021-11-12 20:29:44 +0300

    Online Messaging Module implementation

--- bookings.php
+++ bookings.php
@@ -159,7 +159,6 @@
           <!--/ Col end -->
         </div>
         <!--/ Row end -->
-
     </div>
     <!--/ Container end -->
 
@@ -173,12 +172,12 @@
         <div class="row">
           <div class="col-lg-12">
               <div class="banner-heading">
-                <h1 class="banner-title" style="color: #64dd17">Contact</h1>
+                <h1 class="banner-title" style="color: #64dd17">Messages</h1>
                 <nav aria-label="breadcrumb">
                     <ol class="breadcrumb justify-content-center">
                       <li class="breadcrumb-item"><a href="#" ><b style="color: black">Home </b> </a></li>
                       <li class="breadcrumb-item"><a href="#"  > <b style="color: black"> Company </b></a></li>
-                      <li class="breadcrumb-item active" aria-current="page"  > <b style="color: black">Contact Us </b></li>
+                      <li class="breadcrumb-item active" aria-current="page"  > <b style="color: black">Messages </b></li>
                     </ol>
                 </nav>
               </div>
@@ -188,109 +187,46 @@
   </div><!-- Banner text end -->
 </div><!-- Banner area end --> 
 
-<section id="main-container" class="main-container">
-  <div class="container">
 
-    <div class="row text-center">
-      <div class="col-12">
-        <h2 class="section-title">Reaching our Office</h2>
-        <h3 class="section-sub-title">Find Our Location</h3>
-      </div>
-    </div>
-    <!--/ Title row end -->
+ <?php
+  if(isset($_POST['submit'])){
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $subject = $_POST['subject'];
+        $message = $_POST['message']; 
 
-    <div class="row">
-      <div class="col-md-4">
-        <div class="ts-service-box-bg text-center h-100">
-          <span class="ts-service-icon icon-round">
-            <i class="fas fa-map-marker-alt mr-0"></i>
-          </span>
-          <div class="ts-service-box-content">
-            <h4>Visit Our Office</h4>
-            <p>Repen Complex 3rd Floor, suite 307, Mombasa road </p>
-          </div>
-        </div>
-      </div><!-- Col 1 end -->
+        $to = "info@gleehygienesolutions.co.ke";
+        $subject = 'Message from GLEE HYGIENE SOLUTIONS LTD'." , ";
+        $msg ="Name: ".$name." , "."Email: ".$email." , "." Subject: ".$subject."wrote the following: ".$message;
+        $headers ="From: ".$email;
+         if(mail($to,$subject,$msg,$headers)){
+            
+          echo "<b>Message Sent. Thank You $name for your message.<b>";
 
-      <div class="col-md-4">
-        <div class="ts-service-box-bg text-center h-100">
-          <span class="ts-service-icon icon-round">
-            <i class="fa fa-envelope mr-0"></i>
-          </span>
-          <div class="ts-service-box-content">
-            <h4>Email Us</h4>
-            <p>info@gleehygienesolutions.co.ke</p>
-          </div>
-        </div>
-      </div><!-- Col 2 end -->
+          
+}
 
-      <div class="col-md-4">
-        <div class="ts-service-box-bg text-center h-100">
-          <span class="ts-service-icon icon-round">
-            <i class="fa fa-phone-square mr-0"></i>
-          </span>
-          <div class="ts-service-box-content">
-            <h4>Call Us</h4>
-            <p>+254 724 772 699 | 717 368 442</p>
-          </div>
-        </div>
-      </div><!-- Col 3 end -->
+else {
+    echo "yolo";
+    
+}
 
-    </div><!-- 1st row end -->
-
-    <div class="gap-60"></div>
-
-    <div class="google-map">
-      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3988.6634497341915!2d36.92601481475398!3d-1.3785997989945393!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182f0ce7aa2120e1%3A0x2905bde1b42e68a!2sREPEN%20Complex!5e0!3m2!1sen!2ske!4v1636733359551!5m2!1sen!2ske" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
-      <!-- <div id="map" class="map" data-latitude="40.712776" data-longitude="-74.005974" data-marker="images/marker.png" data-marker-name="Constra"></div> -->
-    </div>
-
-    <div class="gap-40"></div>
-
-    <div class="row">
-      <div class="col-md-12">
-        <h3 class="column-title">We love to hear</h3>
-        <!-- contact form works with formspree.io  -->
-        <!-- contact form activation doc: https://docs.themefisher.com/constra/contact-form/ -->
-        <form  action="message.php" method="post">
-          <div class="error-container"></div>
-          <div class="row">
-            <div class="col-md-4">
-              <div class="form-group">
-                <label>Name</label>
-                <input class="form-control form-control-name" name="name" placeholder="" type="text" required>
-              </div>
-            </div>
-            <div class="col-md-4">
-              <div class="form-group">
-                <label>Email</label>
-                <input class="form-control form-control-email" name="email"  placeholder="" type="email"
-                  required>
-              </div>
-            </div>
-            <div class="col-md-4">
-              <div class="form-group">
-                <label>Subject</label>
-                <input class="form-control form-control-subject" name="subject" placeholder="" required>
-              </div>
-            </div>
-          </div>
-          <div class="form-group">
-            <label>Message</label>
-            <textarea class="form-control form-control-message" name="message"  placeholder="" rows="10"
-              required></textarea>
-          </div>
-          <div class="text-right"><br>
-            <button class="btn btn-primary solid blank" type="submit" name="submit">Send Message</button>
-          </div>
-        </form>
-      </div>
-
-    </div><!-- Content row -->
-  </div><!-- Conatiner end -->
-</section><!-- Main container end -->
+       
 
 
+    }
+    else
+    {
+        echo "try again";
+    }
+?>
+
+
+
+
+
+
+  
   <footer id="footer" class="footer bg-overlay">
     <div class="footer-main">
       <div class="container">
